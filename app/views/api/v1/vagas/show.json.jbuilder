0cee7bf58f3b434a4cd17aad1c79d26fae8b1008
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-14 21:10:09 -0300

    show added

---
+++ app/views/api/v1/vagas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @vaga, :título, :empresa, :nível, :descrição, :local, :url
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :vagas, only: [ :index ]
+      resources :vagas, only: [ :index, :show ]
     end
   end
 end
